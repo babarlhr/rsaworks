ce4543ad4181fc11ac02d20dd3925902d4b4fdec
chadt63 <chadt@ssibtr.com>
2019-10-12 14:58:02 +0000

    Check in 3

--- ssi_nonstock/wizard/sale_nonstock_product.py
+++ ssi_nonstock/wizard/sale_nonstock_product.py
@@ -13,15 +13,54 @@ class SaleNonstockProduct(models.TransientModel):
     partner_id = fields.Many2one('res.partner', string='Vendor', required=True, help="Vendor for Non Stock")
 
     @api.multi
-    @api.returns('self', lambda value: value.id)
     def copy_nonstock(self, default=None):
-#         raise UserError(_(self.product_template_id.name))
-        prod_copy = self.env['product.template'].browse(self.id)
-        line_values = prod_copy.copy()
-        raise UserError(line_values)
-        if default is None:
-            default = {}
-        default['name'] = _("%s (copy)") % self.product_template_id.name
-        return prod_copy.copy(default)
+        active_obj = self.env['sale.order'].browse(self._context.get('active_id'))
+        order_line_obj = self.env['sale.order.line']
+        # Create New NS Product
+        prod_template = self.env['product.template']
+        vals = {
+            'categ_id':self.product_template_id.categ_id,
+            'sale_ok':self.product_template_id.sale_ok,
+            'purchase_ok':self.product_template_id.purchase_ok,
+            'route_ids':self.product_template_id.route_ids,
+            'name':self.product_template_id.name,
+            'type':self.product_template_id.type,
+        }
+        prod_template.create(vals)
+        for rec in self:
+            vals = {
+                'product_id':rec.bundle_id.id,
+                'product_uom_qty':rec.qty,
+                'product_uom':rec.bundle_id.uom_id.id,
+                'price_unit':rec.sale_price,
+                'name':rec.bundle_id.name,
+                'order_id':active_obj.id,
+            }
+            order_line_obj.create(vals)
+            for product in rec.bundle_id.bundle_product_ids:
+                qty = product.qty * rec.qty
+                vals = {
+                    'product_id':product.product_id.id,
+                    'product_uom_qty':qty,
+                    'product_uom':product.uom_id.id,
+                    'price_unit':0.0,
+                    'name':product.product_id.name,
+                    'order_id':active_obj.id,
+                }
+                order_line_obj.create(vals)
+
+        
+        
+        
+        
+        
+        #         raise UserError(_(self.product_template_id.name))
+#         prod_copy = self.env['product.template'].browse(self.id)
+#         line_values = prod_copy.copy()
+#         raise UserError(line_values)
+#         if default is None:
+#             default = {}
+#         default['name'] = _("%s (copy)") % self.product_template_id.name
+#         return prod_copy.copy(default)
 
 
